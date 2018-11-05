2e9e3769438c1efce721c95ea318cd5c0cfe3182
Rain-field <1244082815@qq.com>
2018-11-05 09:58:55 +0800

    1.1我的表单授权我的加以修改 1.2日志管理样式修改

--- info_collect/js/Myform.js
+++ info_collect/js/Myform.js
@@ -1,23 +1,59 @@
-layui.use('laydate', function () {
-    var laydate = layui.laydate;
+var editBtn = $(".edit-btn");
+        layui.use('laydate', function () {
+            var laydate = layui.laydate;
 
-    //常规用法
-    // laydate.render({
-    //     elem: '.date1,.date2'
-    // });
-    
-   
-    //同时绑定多个
-    lay('.date').each(function () {
-        laydate.render({
-            elem: this
-            , trigger: 'click'
+            //同时绑定多个
+            lay('.date').each(function () {
+                laydate.render({
+                    elem: this
+                    , trigger: 'click'
+                    , theme: 'molv'
+                });
+            });
         });
-    });
 
-    //墨绿主题
-    laydate.render({
-        elem: '.date'
-        , theme: 'molv'
-    });
-});
+        //-----------------------------编辑模式----------------------------------------//
+        //点击编辑按钮----进入编辑模式
+        editBtn.on('click', function () {
+            editBtn.hide().siblings('.my-add').show().siblings('.my-done').show();
+            if ($('.edit-range')) {
+                $('.edit-range>i').show();
+                $('.edit-range').removeClass('edit-range-done');
+            }
+        });
+
+        //点击添加按钮
+        $('.myform-mid .my-add').on('click', function () {
+            $(this).before(
+                `<div class='layui-inline edit-range'>
+               <input type="text" name="time" class="layui-input my-input-mwid my-btn-blue">
+               <button class = 'layui-btn  layui-btn-primary layui-btn-sm  my-from-boon my-from-short my-btn-blue my-hide my-input-mwid my-icon-del'></button>
+               <i class='layui-icon  my-btn-blue my-icon-inline'>&#x1007;</i>
+               </div>`
+            );
+            $('.edit-range>input').focus();
+
+        });
+        //判断输入的分类是否为空决定要不要进行渲染还是删除
+        $('.myform-mid').on('blur', '.edit-range>input', function () {
+            if ($(this).val()) {
+                var val = $(this).val();
+                $(this).next().text(val).show();
+                $(this).hide().siblings('i').removeClass('my-btn-blue').addClass('my-btn-danger');
+            } else {
+                $(this).parent().remove();
+            }
+        });
+
+        //删除分类
+        $('.myform-mid').on('click', 'i.my-btn-danger', function () {
+            $(this).parent().remove();
+        })
+
+        //完成编辑
+        //显示edit-btn,隐藏my-done,my-add类，隐藏删除类的标签
+        $('.myform-mid .my-done').on('click', function () {
+            $(this).siblings('.my-add').hide().end().hide().siblings('.edit-btn').show();
+            $('.edit-range>i').hide();
+            $('.edit-range').addClass('edit-range-done');
+        })
